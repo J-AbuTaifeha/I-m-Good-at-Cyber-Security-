04eb5e94e57ab2f505e1fe97a151f5620f4ca0ab
J-AbuTaifeha <oxeto012@outlook.com>
2026-07-29 00:30:49 +0300

    After got hacked commit main.py

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ def check_password_strength():
         
     print(f"\nStrength Score: {score}/5")
     if score == 5:
-        print("Think you are good at cyber security ?! You have just got hacked by Mythos !")
+        print("Great Password!")
     else:
         print("Status: Weak Password. Suggestions:")
         for tip in feedback:
